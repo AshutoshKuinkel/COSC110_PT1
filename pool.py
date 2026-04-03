dcfee40c39d0d9327d17851f78fd253f03c351a3
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-04-03 18:32:22 +1100

    feat: cheapest cost calculation refactor

--- pool.py
+++ pool.py
@@ -1,72 +1,78 @@
+import math
 
 print("--- Codetown Public Pool POS ---")
 determine_if_sale_called = input("Press q to exit or any key to start a new sale: \n")
 
-prices = {
-  "Adult" : 5,
-  "Child" : 4,
-  "Family Pass A" : 16,
-  "Family Pass B" : 16
-}
-  
+# Helper to output purchase receipt:
+def purchase_receipt() -> str :
+  '''
+    Write up docstring for this helper
+  '''
+  pass
 
-def family_pass_a_calculation():
-  print("Receipt: ")
-  print("FAMILY PASS A \t : $Cost for each one ")
-  print("---------------------------")
-  print("TOTAL: $ return total float")
-  print("\n")
-
-def family_pass_b_calculation():
-  print("Receipt:")
-  print("FAMILY PASS B \t : $Cost for each one ")
-  print("---------------------------")
-  print("TOTAL: $ return total")
-  print("\n")
-
-def ordinary_ticket_calculation():
-  print("Receipt:")
-  print("Adult \t : $Cost for each one ")
-  print("---------------------------")
-  print("TOTAL: $ return total")
-  print("\n")
-
-while(determine_if_sale_called != "q"):
+# Helper to get the cheapest cost:
+# run through cases to output final price
+# i.e. make sure we calculate all possible output prices from all possible combinations and select minimum cost
+def get_cheapest_cost(no_of_adults,no_of_children) -> int:
+  '''
+  Write up docstring for this helper
+  '''
+  # setting variables to make final calculations of cheapest cost more inuitive, focusing on core logic/problem at hand and readability rather than few short lines of code:
+  min_cost = math.inf
+  all_possible_nums_of_family_a_passes = no_of_adults//2 + 1 # e.g. 8 adults = 8//2 = 4 bundles possible, + 1 is to include max num...
+  all_possible_nums_of_family_b_passes = no_of_children//3 + 1 # e.g. 3 children = 3//3 = 0 + 1, 1 bundle possible
   
-  try:
-    # Basis of our program, need numbers of adults & children for calculations:
-    no_of_adults = int(input("Enter number of adults: "))
-    no_of_children = int(input("Enter number of chlidren: "))
-    print("\n")
-  except ValueError:
-    # redundant, as python checks itself for int type with int(input()), but task requirements speficially mention:
-    # The input must be an integer. If the user enters text or a float, display an error and ask again.
-    print("Error: Please enter a valid integer using digits for number of adults & children.")
   
-  # input validation; We must have at least one adult for supervision.
-  if(no_of_adults<1):
-    raise ValueError("Error: At least one adult is required for supervision.")
-  if(no_of_children<0):
-    raise ValueError("Error: Number of children cannot be negative.")
   
-  # run through cases to output final tickets
-  # this is wrong, what about when there is e.g. 4 adults and 2 children? we wouldn't return cheapest price with this...
-  # instead i think what we can do is use modulo and check for even and odd numbers, if even that means we have to have at least 2 adults and 2 children, only then we give family pass
-  # this should handle case where we have like 4 adults and 2 children... but even with this modulo approach I need to check the output e.g. when we have 2 adults and 0 children...
-  # maybe the solution is to use the modulo thing, but make sure we have at least 1 children and one adult
-  if( no_of_adults == 2 and no_of_children == 2):
-    family_pass_a_calculation()
-  elif( no_of_adults == 1 and no_of_children == 3):
-    family_pass_b_calculation()
-  else:
-    ordinary_ticket_calculation()
+  for num_of_pass_a in range(all_possible_nums_of_family_a_passes):
+    for num_of_pass_b in range(all_possible_nums_of_family_b_passes):
+      
+      adults_used_for_pass_a = 2 * num_of_pass_a
+      children_used_for_pass_a = 2 * num_of_pass_a
+      
+      adults_used_for_pass_b = 1 * num_of_pass_b
+      children_used_for_pass_b = 3 * num_of_pass_b
+      
+      adults_remaining = no_of_adults - adults_used_for_pass_a - adults_used_for_pass_b
+      children_remaining = no_of_children - children_used_for_pass_a - children_used_for_pass_b
+      
+      # Skipping invalid cases where at least 1 adult dont exist and child is less than 0
+      if ( adults_remaining < 0 or children_remaining < 0):
+        continue
+      
+      # total cost calulation for each case:
+      ttl_cost = 16*num_of_pass_a + 16*num_of_pass_b + 5*adults_remaining + 4*children_remaining
+      
+      # only store minimum cost out of all the costs we calulated:
+      min_cost = min(min_cost,ttl_cost)
+      
+      return min_cost
+
+def main() -> None:
+  while(determine_if_sale_called != "q"):
     
-  
-     
-  # print(f'no_of_adults is {no_of_adults}')
-  # print(f'no_of_children is {no_of_children}')
-  
-  determine_if_sale_called = input("Press q to exit or any key to start a new sale: \n")
-  
+    try:
+      # Basis of our program, need numbers of adults & children for calculations:
+      no_of_adults = int(input("Enter number of adults: "))
+      no_of_children = int(input("Enter number of chlidren: "))
+      print("\n")
+    except ValueError:
+      # redundant, as python checks itself for int type with int(input()), but task requirements speficially mention:
+      # The input must be an integer. If the user enters text or a float, display an error and ask again.
+      print("Error: Please enter a valid integer using digits for number of adults & children.")
+    
+    # input validation; We must have at least one adult for supervision.
+    if(no_of_adults<1):
+      raise ValueError("Error: At least one adult is required for supervision.")
+    if(no_of_children<0):
+      raise ValueError("Error: Number of children cannot be negative.")
+    
+    get_cheapest_cost(no_of_adults,no_of_children)
+    
+    
+    determine_if_sale_called = input("Press q to exit or any key to start a new sale: \n")
 
+
+if __name__ == "__main__":
+  main()
 
